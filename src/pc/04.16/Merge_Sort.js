c35b52c00fcbca6243b6e43fdd77abc96d8fd5b6
彭畅 <1326278155@qq.com>
2025-04-16 16:06:18 +0800

    feat:修改了无限循环的问题,几乎浪费了3秒钟的时间
    --忘记alternate++了

--- src/pc/04.16/Merge_Sort.js
+++ src/pc/04.16/Merge_Sort.js
@@ -9,54 +9,57 @@
  * ✋包是稳定的✋
  */
 export default function mergeSort(arr) {
-  let res = [];
+  let res = arr
+
   function concatSortedArr(arrLeft, arrRight) {
-    if (typeof arrLeft === 'number') {
-      arrLeft = [arrLeft];
+    if (typeof arrLeft === "number") {
+      arrLeft = [arrLeft]
     }
-    if (typeof arrRight === 'number') {
-      arrRight = [arrRight];
+    if (typeof arrRight === "number") {
+      arrRight = [arrRight]
     }
-    let resArr = [];
-    let left = 0;
-    let right = 0;
+    let resArr = []
+    let left = 0
+    let right = 0
     while (left < arrLeft.length && right < arrRight.length) {
       if (arrLeft[left] < arrRight[right]) {
-        left++;
-        resArr.push(arrLeft[left]);
+        resArr.push(arrLeft[left])
+        left++
       } else {
-        resArr.push(arrRight[right]);
-        right++;
+        resArr.push(arrRight[right])
+        right++
       }
     }
     while (right < arrRight.length) {
-      resArr.push(arrRight[right]);
-      right++;
+      resArr.push(arrRight[right])
+      right++
     }
     while (left < arrLeft.length) {
-      resArr.push(arrLeft[right]);
-      left++;
+      resArr.push(arrLeft[left])
+      left++
     }
-    return resArr;
+    return resArr
   }
   function catArr(arr) {
-    let alternate = 1;
-    let pendingArr = [];
-    let resArr = [];
+    let alternate = 1
+    let pendingArr = []
+    let resArr = []
     arr.forEach((element) => {
       if (alternate % 2) {
-        pendingArr = element;
+        pendingArr = element
       } else {
-        resArr.push(concatSortedArr(pendingArr, element));
-        pendingArr = [];
+        resArr.push(concatSortedArr(pendingArr, element))
+        pendingArr = []
       }
-    });
+      alternate++
+    })
     if (pendingArr.length) {
-      resArr.push(concatSortedArr(pendingArr, []));
+      resArr.push(concatSortedArr(pendingArr, []))
     }
-    return resArr;
+    return resArr
   }
   while (res.length !== 1) {
-    res = catArr(arr);
+    res = catArr(res)
   }
+  return res
 }
