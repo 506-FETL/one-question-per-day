33803c059d27c197312b2a6aea5050b1942f9ab8
lll <347552878@qq.com>
2025-04-18 12:37:58 +0800

    refactor: 完成每日一题
    - 实现Vue简化响应式
    - 重命名文件名，更符合规范
    
    Co-Authored-By: Gong Chen Yu <74220172+xiersiki@users.noreply.github.com>

--- problem.js
+++ problem.js
@@ -4,13 +4,13 @@
  * @returns {Proxy} 响应式对象
  */
 function reactive(obj) {
-    // 实现内容
-  }
-  
-  /**
-   * 注册副作用函数，当响应式数据变化时触发它
-   * @param {Function} fn - 依赖响应式数据的函数
-   */
-  function effect(fn) {
-    // 实现内容
-  }
+  // 实现内容
+}
+
+/**
+ * 注册副作用函数，当响应式数据变化时触发它
+ * @param {Function} fn - 依赖响应式数据的函数
+ */
+function effect(fn) {
+  // 实现内容
+}
