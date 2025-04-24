ff50ac79ac9f0549d89b70b1678454c6e5101243
wang-danni <319231518@qq.com>
2025-04-24 14:50:27 +0800

    feat: 完成4.16每日一题

---
+++ src/wdn/04.16/Merge_Sort.js
@@ -0,0 +1,33 @@
+/**
+ * @param {number[]} arr
+ *
+ * 手写[Merge Sort](https://en.wikipedia.org/wiki/Merge_sort)
+ * 请直接修改传入的数组，不要返回新数组。
+ *
+ * 追问
+ * 时间空间复杂度是多少？是否是稳定的排序？
+ */
+export default function mergeSort(arr) {
+    let l = 0;
+    let r = arr.length -1;
+    function merge(arr, l, r){
+        if(l >= r) return;
+        const mid = (l + r) >> 1;
+        merge(arr, l, mid), merge(arr, mid + 1, r);
+        let i = l;
+        let j= mid +1;
+        const temp=[];
+        while(i <= mid && j <= r){
+            if(arr[i] < arr[j]) temp.push(arr[i++])
+            else temp.push(arr[j++]) 
+        }
+        while(i <= mid) temp.push(arr[i++]);
+        while(j<= r) temp.push(arr[j++]);
+        for(let i = l, j = 0 ; i <= r; i++, j++){
+            arr[i] = temp[j];
+        }
+    }
+    merge(arr, l, r);
+
+}
+  
