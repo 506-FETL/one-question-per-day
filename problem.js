afe192e681774408aed5050ca1706888654c7b0a
lll <347552878@qq.com>
2025-04-29 13:42:16 +0800

    feat: 每日一题--实现MiddleWare函数

--- problem.js
+++ problem.js
@@ -1,11 +1,11 @@
-/**
- * 自定义实现 Array.from 方法，将类数组对象或可迭代对象转换为数组。
- *
- * @param {ArrayLike|Iterable} arrayLike - 类数组对象或可迭代对象。
- * @param {Function} [mapFn] - 可选的映射函数，用于对每个元素进行处理。
- * @param {*} [thisArg] - 可选的上下文对象，用于绑定 `mapFn` 的 `this` 值。
- * @returns {Array} 返回一个新数组，包含从 `arrayLike` 转换而来的元素。
- * @throws {TypeError} 如果 `arrayLike` 为 null 或 undefined，抛出类型错误。
- * @throws {TypeError} 如果提供的 `mapFn` 不是函数，抛出类型错误。
- */
-export default function myFrom(arrayLike, mapFn, thisArg) {}
+export default class Middleware {
+  /**
+   * @param {MiddlewareFunc} func
+   */
+  use(func) {}
+
+  /**
+   * @param {Request} req
+   */
+  start(req) {}
+}
