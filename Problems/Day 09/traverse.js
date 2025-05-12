60eeeeca5d7a674efb4c3dfc70b2acd73a619247
彭畅 <1326278155@qq.com>
2025-05-12 17:19:13 +0800

    fix: 更正Problems

--- Problems/Day 09/traverse.js
+++ Problems/Day 09/traverse.js
@@ -1,4 +1,3 @@
-/* eslint-disable no-prototype-builtins */
 // This is the class for the node
 // you can use this directly as it is bundled with your code
 export class Node {
@@ -13,40 +12,4 @@ export class Node {
  * @param {Node} root
  * @returns {number[]}
  */
-export default function traverse(root) {
-  // Base case
-  if (!root) return []
-
-  let nodes = {}
-  let minimum = 0,
-    maximum = 0 // Keep track of min, max x coordinate
-
-  // DFS so we explore the leftmost side to ensure sorted
-  function dfs(node, x, y, parent) {
-    if (!nodes.hasOwnProperty(x)) {
-      nodes[x] = [[node.value, y, parent]]
-    } else {
-      nodes[x].push([node.value, y, parent])
-    }
-
-    minimum = Math.min(minimum, x)
-    maximum = Math.max(maximum, x)
-
-    if (node.left) {
-      dfs(node.left, x - 1, y + 1, x)
-    }
-    if (node.right) {
-      dfs(node.right, x + 1, y + 1, x)
-    }
-  }
-  dfs(root, 0, 0)
-  let res = []
-  for (let i = minimum; i <= maximum; i++) {
-    // Sort by y-value first, then parents' position
-    nodes[i].sort((n1, n2) => {
-      return n1[1] - n2[1] || n1[2] - n2[2]
-    })
-    res = res.concat(nodes[i].map((e) => e[0]))
-  }
-  return res
-}
+export default function traverse(root) {}
