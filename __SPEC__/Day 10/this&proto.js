565a8c3f99303e9ba8c8c3e05f9b9358d441f3a4
lll <347552878@qq.com>
2025-05-12 13:29:46 +0800

    feat: 每日一题Day 10 -- 手写 call、apply、bind、instanceof 和 new

---
+++ __SPEC__/Day 10/this&proto.js
@@ -0,0 +1,84 @@
+// 由于 Node 环境没有 window，用 globalThis 代替
+const window = globalThis
+// 手写call
+Function.prototype.Call = function (context, ...args) {
+  // context为undefined或null时，则this默认指向全局window
+  if (context === undefined || context === null) {
+    context = window
+  }
+  // 利用Symbol创建一个唯一的key值，防止新增加的属性与obj中的属性名重复
+  let fn = Symbol()
+  // this指向调用call的函数
+  context[fn] = this
+  // 隐式绑定this，如执行obj.foo(), foo内的this指向obj
+  let res = context[fn](...args)
+  // 执行完以后，删除新增加的属性
+  delete context[fn]
+  return res
+}
+
+// apply与call相似，只有第二个参数是一个数组，
+Function.prototype.Apply = function (context, args = []) {
+  if (context === undefined || context === null) {
+    context = window
+  }
+  let fn = Symbol()
+  context[fn] = this
+  let res = context[fn](...args)
+  delete context[fn]
+  return res
+}
+
+// bind要考虑返回的函数，作为构造函数被调用的情况
+Function.prototype.Bind = function (context, ...args) {
+  if (context === undefined || context === null) {
+    context = window
+  }
+  let fn = this
+  let f = Symbol()
+  const result = function (...args1) {
+    if (this instanceof fn) {
+      // result如果作为构造函数被调用，this指向的是new出来的对象
+      // this instanceof fn，判断new出来的对象是否为fn的实例
+      this[f] = fn
+      let res = this[f](...args, ...args1)
+      delete this[f]
+      return res
+    } else {
+      // bind返回的函数作为普通函数被调用时
+      context[f] = fn
+      let res = context[f](...args, ...args1)
+      delete context[f]
+      return res
+    }
+  }
+  // 如果绑定的是构造函数 那么需要继承构造函数原型属性和方法
+  // 实现继承的方式: 使用Object.create
+  result.prototype = Object.create(fn.prototype)
+  result.prototype.constructor = result
+  return result
+}
+
+export function instanceOf(obj, fn) {
+  let proto = obj.__proto__
+  if (proto) {
+    if (proto === fn.prototype) {
+      return true
+    } else {
+      return instanceOf(proto, fn)
+    }
+  } else {
+    return false
+  }
+}
+
+export function selfNew(fn, ...args) {
+  // 创建一个instance对象，该对象的原型是fn.prototype
+  let instance = Object.create(fn.prototype)
+  // 调用构造函数，使用apply，将this指向新生成的对象
+  let res = fn.apply(instance, args)
+  // 如果fn函数有返回值，并且返回值是一个对象或方法，则返回该对象，否则返回新生成的instance对象
+  return typeof res === 'object' || typeof res === 'function' ? res : instance
+}
+
+function add() {}
---
+++ src/seam/Day 10/this&proto.js
@@ -0,0 +1,76 @@
+// 由于 Node 环境没有 window，用 globalThis 代替
+const window = globalThis
+
+// call
+Function.prototype.Call = function (context, ...args) {
+  if (context == null) context = window
+
+  const key = Symbol()
+  context[key] = this
+  const result = context[key](...args)
+  delete context[key]
+
+  return result
+}
+
+// apply
+Function.prototype.Apply = function (context, args = []) {
+  if (context == null) context = window
+
+  const key = Symbol()
+  context[key] = this
+  const result = context[key](...args)
+  delete context[key]
+
+  return result
+}
+
+// bind
+Function.prototype.Bind = function (context, ...args) {
+  if (context == null) context = window
+
+  const key = Symbol()
+  const that = this
+
+  const result = function (...newArgs) {
+    if (this instanceof that) {
+      this[key] = that
+      const result = this[key](...args, ...newArgs)
+      delete this[key]
+
+      return result
+    } else {
+      context[key] = that
+      const result = context[key](...args, ...newArgs)
+      delete context[key]
+
+      return result
+    }
+  }
+
+  result.prototype = Object.create(that.prototype)
+  result.prototype.constructor = result
+
+  return result
+}
+
+export function instanceOf(obj, fn) {
+  const proto = obj.__proto__
+
+  if (proto) {
+    if (proto === fn.prototype) {
+      return true
+    } else {
+      return instanceOf(proto, fn)
+    }
+  } else {
+    return false
+  }
+}
+
+export function selfNew(fn, ...args) {
+  const instance = Object.create(fn.prototype)
+  const result = fn.apply(instance, args)
+
+  return typeof result === 'function' || typeof result === 'object' ? result : instance
+}
