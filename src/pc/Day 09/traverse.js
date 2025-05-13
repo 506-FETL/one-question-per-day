969bcef5240ee90f416266f04796f9a32b4726ac
彭畅 <1326278155@qq.com>
2025-05-13 11:56:15 +0800

    feat:完成垂直遍历二叉树

---
+++ src/pc/Day 09/traverse.js
@@ -0,0 +1,46 @@
+// This is the class for the node
+// you can use this directly as it is bundled with your code
+export class Node {
+  constructor(val) {
+    this.value = val
+    this.left = null
+    this.right = null
+  }
+}
+// }
+/**
+ * @param {Node} root
+ * @returns {number[]}
+ */
+export default function traverse(root) {
+  if (!root) return []
+
+  const nodeList = []
+  let index = 0
+
+  function DFS(node, x, y, f) {
+    if (!node) return
+
+    nodeList.push({
+      value: node.value,
+      x: x,
+      y: y,
+      f: f,
+      index: index++, // 记录遍历顺序，用于解决相同坐标的排序
+    })
+
+    DFS(node.left, x - 1, y - 1, x)
+    DFS(node.right, x + 1, y - 1, x)
+  }
+
+  DFS(root, 0, 0)
+
+  nodeList.sort((a, b) => {
+    if (a.x !== b.x) return a.x - b.x
+    if (a.y !== b.y) return b.y - a.y
+    return a.f - b.f
+  })
+
+  // 返回排序后的节点值
+  return nodeList.map((node) => node.value)
+}
