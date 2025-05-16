e36524375fcd017a85121e2db3196f09e4315b23
lll <347552878@qq.com>
2025-05-16 15:23:34 +0800

    feat: 完成Day10

---
+++ src/seam/Day 11/fiberTree.js
@@ -0,0 +1,21 @@
+export function commitNestedComponent(root, onCommitUnmount) {
+  let dep = root
+
+  while (dep) {
+    onCommitUnmount(dep)
+
+    if (dep.child) {
+      dep = dep.child
+      continue
+    }
+
+    if (dep === root) return
+
+    while (!dep.sibling) {
+      if (!dep.return || dep.return === root) return
+      dep = dep.return
+    }
+
+    dep = dep.sibling
+  }
+}
