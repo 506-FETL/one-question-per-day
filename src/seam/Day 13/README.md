2fc2e225d1f9f8b492d89ee77f2ee6c45acfc04f
lll <347552878@qq.com>
2025-05-22 14:08:28 +0800

    feat: Day13--add

---
+++ src/seam/Day 13/README.md
@@ -0,0 +1,10 @@
+## add
+
+实现一个 add 要求能达到一下效果：
+
+```js
+const value = add[100] + 1
+console.log(value) // 101
+console.log(value[200][300] + 100) // 701
+console.log(add[100][200][300] - 300) // 300
+```
--- vitest.config.js
+++ vitest.config.js
@@ -2,7 +2,7 @@ import { configDefaults, defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
-    exclude: [...configDefaults.exclude, 'Problems/*'],
+    exclude: [...configDefaults.exclude, 'Problems/**'],
     coverage: {
       provider: 'v8',
       include: ['src/**'],
