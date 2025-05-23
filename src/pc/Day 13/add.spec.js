aaa61a9d513b5757aab3da87080f48c197bcebf5
彭畅 <1326278155@qq.com>
2025-05-23 11:18:25 +0800

    feat:复习proxy

---
+++ src/pc/Day 13/add.spec.js
@@ -0,0 +1,22 @@
+import { describe, it, expect } from 'vitest'
+import add from './add'
+
+describe('Day 13 -- add', () => {
+  it('应该正常简单相加', () => {
+    const result = add[100]
+    expect(result + 1).toBe(101)
+  })
+
+  it('应该能连续调用', () => {
+    expect(add[100][200] + 100).toBe(400)
+  })
+
+  it('应该能保存状态', () => {
+    const foo = add[100]
+    const result = foo[200]
+
+    expect(foo + 200).toBe(300)
+    expect(result[-100] - 100).toBe(100)
+    expect(result - 200).toBe(100)
+  })
+})
