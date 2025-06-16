2f87512a27e1f4225bf68cd5e5fd0e6b9878d748
彭畅 <1326278155@qq.com>
2025-06-16 19:12:35 +0800

    feat:学会codeReview

---
+++ src/pc/Day 16/addComma.js
@@ -0,0 +1,44 @@
+/**
+ * @param {number} num
+ * @return {string}
+ */
+
+export default function addComma(num) {
+  num = '' + num
+  const { code, getWrapperCode } = getCode(num)
+  let res = ''
+  for (let i = code.length - 1, j = 1; i >= 0; i--, j++) {
+    if (j % 3 === 0 && i !== 0) {
+      res = ',' + code[i] + res
+      continue
+    }
+    res = code[i] + res
+  }
+  return getWrapperCode(res)
+}
+const getCode = (content) => {
+  let front = ''
+  let end = ''
+
+  function getWrapperCode(code) {
+    return front + code + end
+  }
+  function setFrontAndEnd(code) {
+    code = '' + code
+    if (code[0] === '-') {
+      front = '-'
+      code = code.slice(1)
+    }
+    const pointLocation = code.indexOf('.')
+    if (pointLocation !== -1) {
+      end = code.slice(pointLocation)
+      code = code.slice(0, pointLocation)
+    }
+    return code
+  }
+  const code = setFrontAndEnd(content)
+  return {
+    code,
+    getWrapperCode,
+  }
+}
