42afdc5e7f8704495794927d383c98a2aff9772b
lll <87215099+seaeam@users.noreply.github.com>
2025-06-23 21:20:33 +0800

    fix: 增强Day-18测试 - 优化测试断言输出 - 增加 Object.is 的测试边界

--- src/seam/Day 18/myExpect.spec.js
+++ src/seam/Day 18/myExpect.spec.js
@@ -10,9 +10,10 @@ describe('Day18-myExpect', () => {
     [1, 1, true],
     [Symbol(1), Symbol(1), false],
     [Symbol.for(1), Symbol.for(1), true],
+    [+0, -0, false],
     [{}, {}, false],
     [theSameObject, theSameObject, true],
-  ])('myExpect(%j).toBe(%j) 的结果应为 %s', (received, expected, result) => {
+  ])('myExpect(%s).toBe(%s) 的结果应为 %s', (received, expected, result) => {
     expect(myExpect(received).toBe(expected)).toBe(result)
   })
 
@@ -20,10 +21,11 @@ describe('Day18-myExpect', () => {
     [3, 3, false],
     [1, 1, false],
     [Symbol(1), Symbol(1), true],
+    [+0, -0, true],
     [Symbol.for(1), Symbol.for(1), false],
     [{}, {}, true],
     [theSameObject, theSameObject, false],
-  ])('myExpect(%j).not.toBe(%j) 的结果应为 %s', (received, expected, result) => {
+  ])('myExpect(%s).not.toBe(%s) 的结果应为 %s', (received, expected, result) => {
     expect(myExpect(received).not.toBe(expected)).toBe(result)
   })
 })
