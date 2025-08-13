3d807365d0aa31ab5de70cc76b7c7762a47adad9
lll <347552878@qq.com>
2025-08-13 13:33:21 +0800

    feat: 添加 TypeScript 支持及相关配置文件

---
+++ problems/days/Day 01/ts/Merge_Sort.ts
@@ -0,0 +1,10 @@
+/**
+ * @param {number[]} arr
+ *
+ * 手写[Merge Sort](https://en.wikipedia.org/wiki/Merge_sort)
+ * 请直接修改传入的数组，不要返回新数组。
+ *
+ * 追问
+ * 时间空间复杂度是多少？是否是稳定的排序？
+ */
+export default function mergeSort(arr: number[]) {}
