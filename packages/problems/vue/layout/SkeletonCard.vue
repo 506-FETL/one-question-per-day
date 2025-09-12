a95396ca8e2172a3c9581901bc45c803a2347641
lll <347552878@qq.com>
2025-09-12 10:37:12 +0800

    fix: 修复Skeleton组件样式

--- packages/problems/vue/layout/SkeletonCard.vue
+++ packages/problems/vue/layout/SkeletonCard.vue
@@ -5,16 +5,16 @@ import Skeleton from '@/components/ui/skeleton.vue'
 <template>
   <div className="w-full min-h-screen flex items-center justify-center px-4">
     <div className="flex flex-col space-y-6 w-full max-w-md">
-      <Skeleton class-name="h-48 w-full rounded-xl" />
+      <Skeleton class="h-48 w-full rounded-xl" />
       <div className="space-y-3">
-        <Skeleton class-name="h-5 w-3/4" />
-        <Skeleton class-name="h-5 w-2/3" />
-        <Skeleton class-name="h-5 w-1/2" />
+        <Skeleton class="h-5 w-3/4" />
+        <Skeleton class="h-5 w-2/3" />
+        <Skeleton class="h-5 w-1/2" />
       </div>
       <div className="space-y-2">
-        <Skeleton class-name="h-4 w-full" />
-        <Skeleton class-name="h-4 w-5/6" />
-        <Skeleton class-name="h-4 w-2/3" />
+        <Skeleton class="h-4 w-full" />
+        <Skeleton class="h-4 w-5/6" />
+        <Skeleton class="h-4 w-2/3" />
       </div>
     </div>
   </div>
